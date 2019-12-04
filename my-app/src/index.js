92e3c0189690d4562e7e4dbb43ff25a87d227539
winnie-kong <2092280977@qq.com>
2019-12-04 17:15:11 +0800

    react router

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import Cont from './Cont';
 import Footer from './Footer';
+import Ttt from './Ttt';
 import * as serviceWorker from './serviceWorker';
 // 先是引用了App from './App';，
 // 然后获取到了index.html里面定义的id把，  “App from './App'”   放在了index.html里面定义的id
 
 
 ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<Ttt/>, document.getElementById('333'));
 ReactDOM.render(<Cont/>, document.getElementById('tent'));
 ReactDOM.render(<Footer/>, document.getElementById('info'));
 
